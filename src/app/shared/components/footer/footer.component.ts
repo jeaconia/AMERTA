311e26a5447b97512a14c1d1e4b43e31f460a0e5
jeaconia <tjeaconia8@gmail.com>
2026-07-31 14:28:05 +0800

    Added links to footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -17,6 +17,10 @@ export interface SocialLink {
 export class FooterComponent {
   @Input() villageName: string = 'Desa Belok/Sidan';
 
+  getAddressLink(): string {
+    return 'https://share.google/WOzDfNk7hdhWCSa1j';
+  }
+
   @Input() about: string =
     'Lahir dari sejarah penyatuan wilayah pada 1957, Desa Belok/Sidan kini bertransformasi menjadi dataran tinggi yang mengubah hamparan ladang asparagus premium menjadi penggerak utama ekonomi warga.';
 
@@ -28,10 +32,9 @@ export class FooterComponent {
 
   /** Daftar tautan social media, default sesuai referensi desain */
   @Input() socialLinks: SocialLink[] = [
-    { platform: 'instagram', url: '#' },
-    { platform: 'facebook', url: '#' },
-    { platform: 'youtube', url: '#' },
-    { platform: 'twitter', url: '#' },
+    { platform: 'instagram', url: 'https://www.instagram.com/beloksidan01/' },
+    { platform: 'facebook', url: 'https://web.facebook.com/pages/Desa%20Belok-Sidan%20Kec.%20Petang/528585030968969/' },
+    { platform: 'youtube', url: 'https://youtu.be/6uN8YUNKpCA?si=YGAaTR45H4oJP1iD' },
   ];
 
   @Input() copyrightText: string = 'Copyright © 2026 Pemerintah Kabupaten Badung. All rights reserved.';
